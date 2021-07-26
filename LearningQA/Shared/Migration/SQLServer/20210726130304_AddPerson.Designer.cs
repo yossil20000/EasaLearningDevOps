aa24ab583e1f98b90dad2ae6923aebfb5b956086
yosef <yos.1965@gmail.com>
2021-07-26 16:10:14 +0300

    Add Person Migration To SqlServer

--- LearningQA/Shared/Migration/SQLServer/20210726130304_AddPerson.Designer.cs
+++ LearningQA/Shared/Migration/SQLServer/20210726130304_AddPerson.Designer.cs
@@ -4,14 +4,16 @@ using LearningQA.Shared.Entities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
-namespace LearningQA.Shared.Migration.SQLight
+namespace LearningQA.Shared.Migration.SQLServer
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20210726130304_AddPerson")]
+    partial class AddPerson
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
